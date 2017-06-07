684a163899a04b823eafba1a7ef5332baa645193
Andrew Arpasi <andrew@andrewarpasi.com>
2017-06-07 16:07:20 -0400

    setup submit handler for dino form

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log("app.js")
+const app = {
+    init(formSelector) {
+        document
+            .querySelector(formSelector)
+            .addEventListener('submit', this.addDino)
+        
+    },
+
+    addDino(event) {
+        event.preventDefault()
+        const dinoName = event.target.dinoName.value
+        console.log(dinoName)
+    },
+}
+
+app.init('#dino-form')
